9786abf2925674448b2446492ee6fafd1530c7be
Francisco Soliz <francisco-soliz@hotmail.com>
2025-07-17 21:33:39 -0500

    cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World con NestJS modificamos!';
+    return 'Hello World con NestJS Tiendita esta online! version: 2025.07.17.21.33';
   }
 }
